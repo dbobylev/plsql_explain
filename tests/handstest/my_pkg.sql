f7dbe755c0b23478d893186587507c4a3d8b3740
dbobylev <netlighht4me@gmail.com>
2026-03-28 10:25:43 +0300

    fix handtest sql

---
+++ tests/handstest/my_pkg.sql
@@ -0,0 +1,40 @@
+create or replace package body myschema.mypackage as
+
+    vNum number;
+
+    procedure test_procedure(pName in varchar2, pVal in number) is
+        vRes number;
+        vUpdated date;
+    begin
+        vRes := myschema.package2.proc2(pName, pVAl);
+
+        begin
+            for i in (
+                select updated 
+                into vUpdated 
+                from myschema.table2 t, 
+                    table2 b 
+                where t.id = vRes 
+                and b.id = t.bid) 
+            loop
+
+                vRes:= 100;
+                if vRes < 3 then
+                    myschema2.package2.proc3(pName, pVAl);
+                elsif vRes < 300 then
+                    myschema3.package4.proc4(pName, pVAl);
+                else
+                    myschema3.package4.proc4(pName, 999);
+                end if;
+                vres := 200;
+
+            end loop;
+            
+            vRes := null;
+        exception
+            when no_data_found then
+            package3.log('error while call test_procedure, pName: ' || pName);
+            raise;
+        end;
+
+    end;
